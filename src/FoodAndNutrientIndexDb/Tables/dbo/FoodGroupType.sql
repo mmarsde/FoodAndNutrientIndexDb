56acf33fcafdbe08e1e0acdebebb9ac6c7babced
mmarsde <57989672+mmarsde@users.noreply.github.com>
2022-11-04 10:45:16 +0000

    Update FoodGroupType.sql
    Fixed formatting

--- src/FoodAndNutrientIndexDb/Tables/dbo/FoodGroupType.sql
+++ src/FoodAndNutrientIndexDb/Tables/dbo/FoodGroupType.sql
@@ -1,6 +1,6 @@
-﻿CREATE TABLE [dbo].[FoodGroupType]
+CREATE TABLE [dbo].[FoodGroupType]
 (
-	[FoodGroupTypeId] INT IDENTITY(1, 1) NOT NULL, 
-    [Description] VARCHAR(50) NULL,
-	CONSTRAINT [PK_FoodGroupType_FoodGroupTypeId] PRIMARY KEY CLUSTERED([FoodGroupTypeId])
+  [FoodGroupTypeId] INT IDENTITY(1, 1) NOT NULL, 
+  [Description] VARCHAR(50) NULL,
+  CONSTRAINT [PK_FoodGroupType_FoodGroupTypeId] PRIMARY KEY CLUSTERED([FoodGroupTypeId])
 );
